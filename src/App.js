6da6f8ebb8cc77907711689142681f759b93e70c
Vova <90705859+VolodymyrKolisnichenko@users.noreply.github.com>
2023-05-09 18:41:55 +0200

    imported
    imported css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  <div>
+    <div className="game"></div>
+    <div className="dino"></div>
+    <div className="cactus"></div>
+  </div>
+)
+
 }
 
 export default App;
+
+{/* document.addEventListener("keydown", function(event) {
+  jump();
+});
+function jump () {
+  if (dino.classList !="jump") {
+      dino.classList.add("jump")
+  }
+  setTimeout(function() {
+      dino.classList.remove("jump")
+  }, 300)
+}
+let isAlive = setInterval (function() {
+  let dinoTop = parseInt (window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt (window.getComputedStyle(cactus).getPropertyValue("left"));
+
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >=140) {
+      alert ("GAME OVER!!")
+  }
+}, 10)
+
+return (
+  <div className="game">
+  <div className="dino"></div>
+  <div className="cactus"></div>  
+</div>
+); */}
